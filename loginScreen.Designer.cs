251fe23b43aab484916ae137d4a550a36a9343b1
Xman9217 <xaviergmitchell13up@gmail.com>
2023-12-04 15:14:53 -0500

    Logging In

--- loginScreen.Designer.cs
+++ loginScreen.Designer.cs
@@ -32,43 +32,54 @@
             this.usernameBox = new System.Windows.Forms.TextBox();
             this.passwordBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // usernameBox
             // 
-            this.usernameBox.Location = new System.Drawing.Point(123, 74);
+            this.usernameBox.Location = new System.Drawing.Point(164, 91);
             this.usernameBox.Margin = new System.Windows.Forms.Padding(1);
             this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(97, 20);
+            this.usernameBox.Size = new System.Drawing.Size(128, 22);
             this.usernameBox.TabIndex = 0;
             this.usernameBox.Text = "Username";
             // 
             // passwordBox
             // 
-            this.passwordBox.Location = new System.Drawing.Point(124, 93);
+            this.passwordBox.Location = new System.Drawing.Point(165, 114);
             this.passwordBox.Margin = new System.Windows.Forms.Padding(1);
             this.passwordBox.Name = "passwordBox";
-            this.passwordBox.Size = new System.Drawing.Size(97, 20);
+            this.passwordBox.Size = new System.Drawing.Size(128, 22);
             this.passwordBox.TabIndex = 1;
             this.passwordBox.Text = "Password";
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(142, 42);
+            this.label1.Location = new System.Drawing.Point(189, 52);
             this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.Size = new System.Drawing.Size(87, 21);
             this.label1.TabIndex = 2;
             this.label1.Text = "Login";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(165, 161);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(128, 41);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            // 
             // loginScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(343, 202);
+            this.ClientSize = new System.Drawing.Size(1123, 550);
+            this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.passwordBox);
             this.Controls.Add(this.usernameBox);
@@ -77,6 +88,7 @@
             this.Text = "Login Screen";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.TextBox usernameBox;
@@ -84,5 +96,7 @@
         private System.Windows.Forms.Label label1;
 
         #endregion
+
+        private System.Windows.Forms.Button loginBtn;
     }
 }
