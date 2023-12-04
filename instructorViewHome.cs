af18b65800e844ead96b3436c6a4ec5f91de8699
Jake <jholbein300@gmail.com>
2023-12-04 01:09:36 -0500

    Adding all the screens (not with many of the buttons and boxes though)

---
+++ instructorViewHome.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Course_Management_System_Final
+{
+    public partial class instructorViewHome : Form
+    {
+        public instructorViewHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
