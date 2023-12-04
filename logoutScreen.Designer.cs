afcba7aa4878b9e15b304f1462da4248e48d76a9
Jake <jholbein300@gmail.com>
2023-12-04 01:34:54 -0500

    Added logout screen

---
+++ logoutScreen.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+
+namespace Course_Management_System_Final
+{
+    partial class logoutScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logoutLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // logoutLabel
+            // 
+            this.logoutLabel.Location = new System.Drawing.Point(234, 153);
+            this.logoutLabel.Name = "logoutLabel";
+            this.logoutLabel.Size = new System.Drawing.Size(100, 23);
+            this.logoutLabel.TabIndex = 0;
+            this.logoutLabel.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(157, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You have successfully logged out.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(205, 200);
+            this.usernameBox.Margin = new System.Windows.Forms.Padding(1);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(97, 20);
+            this.usernameBox.TabIndex = 2;
+            this.usernameBox.Text = "Username";
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(205, 222);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(1);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(97, 20);
+            this.passwordBox.TabIndex = 3;
+            this.passwordBox.Text = "Password";
+            // 
+            // logoutScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logoutLabel);
+            this.Name = "logoutScreen";
+            this.Text = "logoutScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label logoutLabel;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
